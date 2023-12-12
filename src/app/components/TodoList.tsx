3501df51451359f63d99dd32997d57f99a14756b
t_inoue <t.artwork.ceramica@gmail.com>
2023-12-13 00:54:20 +0900

    modify: tailwind setting and TodoList styling
    - tailwind の自動補完設定を追記
    - TodoList のレイアウトスタイルを追加

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const TodoList = () => {
-    return <div>TodoList</div>;
+    return (
+        <ul className="space-y-3 text-black">
+            <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
+                <span className="text-black">散歩</span>
+                <div>
+                    <button className="text-green-400 mr-3">Edit</button>
+                    <button className="text-red-500">Delete</button>
+                </div>
+            </li>
+            <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
+                <span className="text-black">散歩</span>
+                <div>
+                    <button className="text-green-400 mr-3">Edit</button>
+                    <button className="text-red-500">Delete</button>
+                </div>
+            </li>
+            <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
+                <span className="text-black">散歩</span>
+                <div>
+                    <button className="text-green-400 mr-3">Edit</button>
+                    <button className="text-red-500">Delete</button>
+                </div>
+            </li>
+        </ul>
+    );
 };
 
 export default TodoList;
