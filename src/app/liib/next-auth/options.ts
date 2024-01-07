d20ddde93611c8df4bcbf71ac61af84e2db23124
t_inoue <t.artwork.ceramica@gmail.com>
2024-01-08 00:19:16 +0900

    add: next-auth setting
    - setting options provider (GitHub)

---
+++ src/app/liib/next-auth/options.ts
@@ -0,0 +1,12 @@
+import { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+export const nextAuthOptions: NextAuthOptions = {
+    debug: false,
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID!,
+            clientSecret: process.env.GITHUB_SECRET!,
+        }),
+    ],
+};
